63b65b86557f4d70a3468eae322e117458a50859
Jonathan Taylor <jonathantaylor@Jonathans-MacBook-Pro.local>
2021-05-24 20:15:58 -0230

    Update home page background

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,26 +1,9 @@
-import {React, useState} from 'react'
 import './Home.css'
-import Modal from 'react-bootstrap/Modal'
-import Contact from '../Contact'
 
 const Home = () => {
-    const [contact, setContact] = useState(false);
-
-    const contactClose = () => setContact(false);
-    const contactShow = () => setContact(true);
-
     return (
         <div id='Home'>
             <h1 className='name'>Jonathan Taylor</h1>
-            <h3 className='intro'>A Fourth Year Computer Engineering Student</h3>
-            <button className='button' onClick={contactShow}>Contact</button>
-
-            <Modal centered show={contact} onHide={contactClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title style={{textAlign: 'center'}}>Send an email with the form below or directly to <a href='mailto: jttaylor@mun.ca'>jttaylor@mun.ca</a></Modal.Title>
-                </Modal.Header>
-                <Modal.Body><Contact /></Modal.Body>
-            </Modal>
         </div>
     )
 }
